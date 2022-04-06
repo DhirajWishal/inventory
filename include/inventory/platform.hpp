667acb99b92aead6b10f30c793e7c4a70ae89d35
wishaldhiraj@gmail.com <wishaldhiraj@gmail.com>
2022-04-06 13:10:23 +0530

    Lessen some standard restrictions.
    Removed standard restrictions of C++20 as not all compilers support it completely.

--- include/inventory/platform.hpp
+++ include/inventory/platform.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
-// We have to drop nodiscard for clang.
-#if defined(__clang__)
-#define INV_NODISCARD
+#if defined(__APPLE__) || defined(__MACH__) || defined(__clang__)
+#	define INV_NODISCARD
 
 #else
-#define INV_NODISCARD [[nodiscard]]
-
-#endif
-
-// Also, make sure that we do not compile in anything less than C++20.
-#if __cplusplus < 202002L
-#error This library requires C++20!
+#	define INV_NODISCARD	[[nodiscard]]
 
 #endif
