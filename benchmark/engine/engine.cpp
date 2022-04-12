740fdded218baaa96308478e30a4283e91889bb6
Dhiraj Wishal <wishaldhiraj@gmail.com>
2022-04-13 02:08:34 +0530

    Updated the benchmark code for a more leveled testing, and the Ubuntu workflow now uses clang.

--- benchmark/engine/engine.cpp
+++ benchmark/engine/engine.cpp
@@ -6,31 +6,29 @@ namespace engine
 {
 	void engine::update()
 	{
-		for (auto &models : m_ModelComponents)
+		for (auto& models : m_ModelComponents)
 		{
 			update_component(models.component());
-			if (models.entity().is_registered_to<camera_component>())
-				update_component(m_CameraComponents.get(models.entity()));
+			update_component(m_CameraComponents.get(models.entity()));
+			[[maybe_unused]] volatile int x = 0;
 		}
 
-		for (auto &position : m_PositionComponents)
+		for (auto& position : m_PositionComponents)
 		{
 			update_component(position.component());
+			[[maybe_unused]] volatile int x = 0;
 		}
 	}
 
-	void engine::update_component(const model_component &component) const
+	void engine::update_component(const model_component& component) const
 	{
-		[[maybe_unused]] volatile int x = 0;
 	}
 
-	void engine::update_component(const camera_component &component) const
+	void engine::update_component(const camera_component& component) const
 	{
-		[[maybe_unused]] volatile int x = 0;
 	}
 
-	void engine::update_component(const position_component &component) const
+	void engine::update_component(const position_component& component) const
 	{
-		[[maybe_unused]] volatile int x = 0;
 	}
 }
